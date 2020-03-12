fca0694e1b521abb50a7f959919b12d6b851f14f
myname21-meet <myname21@meet.mit.edu>
2020-03-12 19:29:22 +0200

    added app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect, url_for, render_template
+from flask import session as login_session
+
+app = Flask(__name__)
+app.secret_key = "MY_SUPER_SECRET_KEY"
+
+
+@app.route("/")
+def home():
+	return "hello"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
