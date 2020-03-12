6fd059cf3023f71e5e05bf7528571bd74f703a57
myname21-meet <myname21@meet.mit.edu>
2020-03-12 14:01:41 -0400

    helloooo

--- model.py
+++ model.py
@@ -1,5 +1,36 @@
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, Float, Boolean, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
+'''
+
+id
+name
+specielty
+lawyer/mediator
+location
+experience
+education
+'''
+
+class Lawyer(Base):
+	__tablename__='lawyers'
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	profession = Column(String)#lawyer or mediator
+	specielty = Column(String)
+	education = Column(String)
+	experience = Column(String)
+	location = Column(String)
+
+
+class User(Base):
+	__tablename__='lawyers'
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+	email = Column(String)
+	password = Column(String)
+
+
+
 
 Base = declarative_base()
