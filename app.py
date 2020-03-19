30ee638c1b4ec945c5316bbf8933e0cb159442e1
unknown <or20@meet.mit.edu>
2020-03-19 19:03:20 +0200

    hello

--- app.py
+++ app.py
@@ -7,7 +7,15 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 @app.route("/")
 def home():
-	return "hello"
+	return url_for("home.html")
+
+@app.route("/lawyers")
+def lawyers():
+	return url_for("lawyers.html")
+
+@app.route("/rights")
+def rights():
+	return url_for("rights.html")
 
 
 if __name__ == '__main__':
